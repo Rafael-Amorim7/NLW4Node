724666a3ef1181eddd4578b4fa5e3eefe24ff135
Rafael-Amorim7 <rafaamorim1504@gmail.com>
2021-02-26 12:01:58 -0300

    Adicao do find para listar todos os surveys

--- api/src/router.ts
+++ api/src/router.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import { UserController } from './controller/UserController';
 import { SurveysController } from './controller/SurveysController';
 
-
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 
 router.post("/users", userController.create);
-router.post("/surveys", surveysController.create)
+
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
